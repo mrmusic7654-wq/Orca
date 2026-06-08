58a2284cc6ca630e1c30d03fd10b2166003ec082
mrmusic7654-wq <mr.music7654@gmail.com>
2026-06-08 19:51:55 +0545

    Update OrcaApplication.kt

--- orca/app/src/main/java/com/orca/agent/OrcaApplication.kt
+++ orca/app/src/main/java/com/orca/agent/OrcaApplication.kt
@@ -1,1 +1,24 @@
-// app/src/main/java/com/orca/agent/OrcaApplication.kt - REPLACE WITH REAL CONTENT
+package com.orca.agent
+
+import android.app.Application
+import com.orca.agent.core.OrcaCore
+import dagger.hilt.android.HiltAndroidApp
+import javax.inject.Inject
+
+@HiltAndroidApp
+class OrcaApplication : Application() {
+
+    @Inject
+    lateinit var orcaCore: OrcaCore
+
+    override fun onCreate() {
+        super.onCreate()
+        instance = this
+        orcaCore.initialize()
+    }
+
+    companion object {
+        lateinit var instance: OrcaApplication
+            private set
+    }
+}
